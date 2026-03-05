a26f124f361cb98076997fbae642eebb7cd29962
talmo <talmo@talmolab.org>
2026-03-05 22:33:40 +0000

    Deploying to gh-pages from @ talmolab/sleap-io.js@1d30f8fff2ee71f622801965c2f1dd493e64ae77 🚀

--- assets/dist/lite.d.ts
+++ assets/dist/lite.d.ts
@@ -1,5 +1,5 @@
-import { S as Skeleton, T as Track } from './instance-DzZInuvQ.js';
-export { E as Edge, N as Node, j as Symmetry } from './instance-DzZInuvQ.js';
+import { k as Skeleton, T as Track } from './instance-BmOdR704.js';
+export { E as Edge, N as Node, S as Symmetry } from './instance-BmOdR704.js';
 
 /**
  * jsfive-based HDF5 file interface.
